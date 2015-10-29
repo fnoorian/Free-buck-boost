47bcf4d728c714250dbfa1f03ffa4fa08a76fd29
Farzad Noorian <farzad.noorian@gmail.com>
2015-10-29 14:20:53 +1100

    JSON Client: Added dispatcher class and refactored code

--- json_client/fcc_client.py
+++ json_client/fcc_client.py
@@ -1,13 +1,13 @@
-from rpc_base import RPCClientBase
+from rpc_base import JSONRPCClientBase
 
 
-fcc = RPCClientBase("http://127.0.0.1:4002/jsonrpc")
+fcc = JSONRPCClientDispatcher("http://127.0.0.1:4002/jsonrpc")
 
-print(fcc.call("mightywatt_readstatus"))
-print(fcc.call("charger_readstatus"))
-print(fcc.call("discharger_readstatus"))
-print(fcc.call("mppt_readstatus"))
+print(fcc.mightywatt_readstatus())
+print(fcc.charger_readstatus())
+print(fcc.discharger_readstatus())
+print(fcc.mppt_readstatus())
 
-#fcc.call("mightywatt_setpower", 2) # set load power to 2 W.
+#fcc.mightywatt_setpower(2) # set load power to 2 W.
 
 
--- json_client/rpc_base.py
+++ json_client/rpc_base.py
@@ -16,18 +16,41 @@ def enable_stdout_logging():
     ch.setFormatter(formatter)
     root.addHandler(ch)
 
-class RPCClientBase:
+class JSONRPCClientException(Exception):
+    def __init__(self, code, message):
+        self.code = code
+        self.message = message
+
+    def __str__(self):
+        return "RPC Error {0}: {1}".format(self.code, self.message)
+
+class JSONRPCClientBase:
     def __init__(self, url):
         self.url = url
         self.headers = {'content-type': 'application/json'}
 
-    def call(self, method, *kargs, id=None):
+    def build_params(self, args, kwargs):
+        """Build list of parameters from the given arguements"""
+        params = []
+        if len(args) > 0:
+            params = list(args)
+            if len(kwargs) > 0:
+                ValueError("Can't use named and unnamed args in the same function")
+
+        if len(kwargs) > 0:
+            params = dict(kwargs)
+
+        return params
+
+    def callp(self, method, params, rpc_id=None):
+        """RPC call"""
+
         # build payload
         payload = {
             "method": method,
-            "params": list(kargs),
+            "params": params,
             "jsonrpc": "2.0",
-            "id": id,
+            "id": rpc_id,
         }
 
         logging.debug("URL: {0}".format(self.url))
@@ -35,12 +58,58 @@ class RPCClientBase:
         logging.debug(payload)
 
         # send response
-        response = requests.post(self.url, data=json.dumps(payload), headers=self.headers,
+        response = requests.post(self.url, data=json.dumps(payload),
+                                 headers=self.headers,
                                  proxies={"http":""})
 
         logging.debug("Response: ")
         logging.debug(response.content)
+
+        # convert string to json        
+        json_response = response.json()
         logging.debug(response.json())
 
-        return response.json()["result"]
+        # check and raise RPC errors
+        possible_error = json_response.get("error") 
 
+        if possible_error is not None:
+            raise RPCClientException(possible_error["code"], possible_error["message"])
+
+        # return result
+        return json_response["result"]
+
+    def call(self, _method, *args, _rpc_id=None, **kwargs):
+        return self.callp(_method,
+                          self.build_params(args, kwargs),
+                          _rpc_id)
+
+class JSONRPCClientDispatcher:
+
+    def __init__(self, url):
+        self._rpc = JSONRPCClientBase(url)
+
+    def __getattribute__(self, name):
+        if name.startswith('_'):
+            return object.__getattribute__(self, name)
+        else:
+            def dynamic_function(*args, **kwargs):
+                params = self._rpc.build_params(args, kwargs)
+                return self._rpc.callp(name, params)
+
+            return dynamic_function
+
+def rpc_test():
+    #enable_stdout_logging()
+    r0 = JSONRPCClientBase('http://10.65.196.249:4000/jsonrpc')
+    print(r0.call('get_version'))
+    print(r0.call('forecast_demand', start_datetime = "2015-03-14", N = 10, method = "prescient"))
+
+
+    r = JSONRPCClientDispatcher('http://10.65.196.249:4000/jsonrpc')
+    print(r.get_version())
+    print(r.forecast_demand("2015-03-14", 10, "prescient"))
+    print(r.forecast_demand(start_datetime = "2015-03-14", N = 10, method = "prescient"))
+    try:
+        r.forecast_demand(start_datetime = "2015-03-14", N = 10, methodx = "prescient")
+    except RPCClientException as e:
+        print(e)
